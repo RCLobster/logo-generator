4761a58d59d510c30de2d88bd3205bd96b0881a2
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-01-22 15:16:23 -0800

    all propmts complete

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+/*
+THE PLAN
+1. take input for all parameters
+*/
+
 // save dependencies as variables
 const inquirer = require('inquirer');
 const jest = require('jest');
 
 inquirer
-    .createPromptModule([
+    .prompt([
         {
             type: 'input',
             name: 'text',
             message: 'Give me three letters for your logo.'
         },
-        
+        {
+            type: 'input',
+            name: 'txtColor',
+            message: 'What color would you like your letters to be?'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Pick a shape:',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color would you like your shape to be?'
+        }
+
     ])
+    .then((answers) => {
+        console.log(answers);
+    });
